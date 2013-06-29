8547c099b25c4e6f2aba8049eed7a4f2897c706e
Darren Manners <darrenmanners@gmail.com>
2013-06-29 21:54:00 +1000

    seperating api out into ss routes.

---
+++ routes/api.js
@@ -0,0 +1,36 @@
+BeerProvider = require('../beerprovider').BeerProvider;
+var bp = new BeerProvider('localhost', 27017);
+
+exports.beers = {};
+
+exports.beers.get = function(req, res){
+    var id = req.params.id;
+    if(id){
+        console.log('this request has an id attached to it')
+
+    }
+    else {
+        console.log('get all beers')
+        bp.findAll(function(error, beers){
+            res.json({
+                beers: beers
+            })
+        });
+    };
+};
+
+exports.beers.post = function(req, res){
+    res.render('TODO: POST');
+};
+
+exports.beers.delete = function(req, res){
+    res.render('TODO: DELETE')
+}
+
+exports.beers.put = function(req, res){
+    bp.save({
+        entry: req.body
+    }, function(error, docs) {
+        res.redirect('/');
+    })
+};
